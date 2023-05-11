74697ad1526a04c37c8942481c5e1dc3440a5cd9
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-05-11 20:44:44 +0530

    Made the custom hook generic to allow both forward and backward counting

--- src/components/Counters/BackwardCounter.js
+++ src/components/Counters/BackwardCounter.js
@@ -1,17 +1,9 @@
-import { useState, useEffect } from 'react';
-
 import Card from '../UI/Card';
+import useCounter from '../../hooks/use-counter';
 
 const BackwardCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter - 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const forward = false;
+  const counter = useCounter(forward);
 
   return <Card>{counter}</Card>;
 };
